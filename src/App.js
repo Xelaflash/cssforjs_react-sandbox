987d3d99d4ca126a527b979a3e17118ad8410cbf
Alexandre Gros <xelaflash@gmail.com>
2021-10-19 10:55:20 -0400

    add styled component + babel plugin && change header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components/macro";
+
+const HeaderStyles = styled.header`
+  text-align: center;
+  font-weight: bold;
+  margin: 20px auto;
+  font-weight: 600;
+  font-size:1.5rem;
+`;
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <HeaderStyles>
         <p>
           React Sandbox For Css for Js course
         </p>
 
-      </header>
+      </HeaderStyles>
     </div>
   );
 }
